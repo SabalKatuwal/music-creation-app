654785ed8e7fd1c2ebce9c4187e0f771afdbd256
SabalKatuwal <sabal.katuwal1@gmail.com>
2026-02-25 17:14:25 +0545

    Initial Commit

---
+++ MusicCreationApp/MusicCreationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicCreationAppApp.swift
+//  MusicCreationApp
+//
+//  Created by Sabal on 2/25/26.
+//
+
+import SwiftUI
+
+@main
+struct MusicCreationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
